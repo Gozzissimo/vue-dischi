8e27a983bfd94f9b2c52a6dd78033584f121fc7c
Gozzissimo <fantini.nico@gmail.com>
2022-01-18 18:24:15 +0100

    option dinamiche (not singleton)

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -6,19 +6,40 @@
             name="genres" 
             id="genres">
                 <option value="">All</option>
-                <option value="Rock">Rock</option>
-                <option value="Pop">Pop</option>
+                <option 
+                    v-for="(card, index) in cards"
+                    :key="index" 
+                    :value="card.genre">
+                        {{ card.genre }}
+                </option>
+
+                <!-- <option value="Rock">Rock</option>
+                <option value="Pop">Pop</option> -->
         </select>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: "Select",
     data() {
         return {
             selectedGenre: '',
+            cards: null,
         };
+    },
+
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((result) => {
+                console.log(result);
+                this.cards = result.data.response;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     },
 }
 </script>
